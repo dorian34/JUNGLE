3f3f5cf19fe719c8c313a8213695fde840387735
keiflin dorian <dorian.keiflin@gmail.com>
2012-12-08 01:57:52 +0100

    jungle
    communication avec client presque terminé au niveau php

---
+++ PéHpé/Joueur.php
@@ -0,0 +1,218 @@
+﻿<?php
+	/*----------------------------------------------------------------JOUEUR--------------------------------------------------------------------------------*/
+	
+	class Joueur{
+		
+		private $nom;//le nom definitif affecté à ce joueurs sur le serveur comme le client (0,1,2,3)
+		private $socket;
+		private $cardsHide = array();
+		private $cardsKnown = array();
+		private $actualCard = 0;
+		
+		public function __construct($sock,$nom){
+			$this->socket = $sock;
+			$this->nom = $nom;
+		}
+		
+		/****************************************SETTEUR / GETTEUR******************************************************/
+		
+		public function getNom(){
+			return $this->nom;
+		}
+		
+		/*
+		* Setteur carte
+		*/
+		public function setCardsHide($carte = array()){
+			$this->cardsHide = $carte;
+		}
+		
+		/*
+		* Getteur carte
+		*/
+		public function getCardsHide(){
+			return $this->cardsHide;
+		}
+		
+		/*
+		* Setteur carte connnu
+		*/
+		public function setCardsKnown($cardKnown){
+			if(is_array($cardKnown)){
+				for($i=0;$i>count($cardKnown);$i++){
+					array_push($this->cardsKnown,$cardKnown);
+				}
+			}else array_push($this->cardsKnown,$cardKnown);
+		}
+		
+			/*
+		* Getteur carte connnu
+		*/
+		public function getCardsKnown(){
+			$cards = array();
+			$cards =  $this->cardsKnown;
+			
+			/*on supprime les carte retourné*/
+			$this->cardsKnown = array();
+			return $cards;
+		}
+		
+		/*
+		* Getteur socket
+		*/
+		public function getSocket(){
+			return $this->socket;
+		}
+		
+		/*
+		* getCarteDecouverte
+		* retourne le nombre de carte decouverte
+		*/
+		public function getNbDiscoverCard(){
+			return count($this->cardsKnown);
+		}
+		
+		public function getNbHideCard(){
+			return count($this->cardsHide);
+		}
+		
+		/**
+		* Met à jours la carte actuelement devoilé
+		* param : la carte qui vient d'etre tiré
+		*/
+		public function setActualCard($card){
+			$this->actualCard = $card;
+		}
+		
+		/**
+		* Renvoie la carte actuelement devoilé
+		* retour : la carte actuelement devoilé 
+		*/
+		public function getActualCard(){
+			return $this->actualCard;
+		}
+		
+		/*
+		* renvoie le nb total de carte
+		*/
+		public function getAllCards(){
+			return $this->getNbDiscoverCard() + $this->getNbHideCard();
+			
+		}
+		
+		/*
+		* met à jours les cartes des joueurs;
+		*/
+		public function setAfterCatch($cardKnown){
+			$this->setCardsHide(array_merge($this->getCardsKnown(),$cardKnown));
+		}
+		
+		/*****************************************************************************************************/
+		
+		/**
+		* Tire une Carte et met a jours le paquet des cartes connu, supprime la carte tiré du paquet caché
+		* return Renvoie la carte tiré
+		*/
+		public function drawCard(){
+			$carte="";
+			/*si on encore des carte dans notre paquet de carte caché*/
+			if(!empty($this->cards)){
+				/*on met la premiere carte caché dans le paquet de carte devoilé*/
+				$carte = array_shift($this->cards);
+				array_push($this->cardsKnown,$carte);
+				$this->setActualCard($carte);
+			}
+		}
+		
+		/*
+		* renvoie le message à envoyer
+		* Param : le joueurActif
+		*/
+		public function getMessageInit($joueurActif=0){
+			$message="";
+			if($this->getNom() == $joueurActif){
+				$message=$this->getNom()."-PLAY";
+			}else $message=$this->getNom();
+			return $message;
+		}
+		
+		/*
+		* renvoie le message à envoyer
+		* Param : joueurSuivant = 0,1,2,3 ; et la collection de tous les autres joueurs
+		* Return String, chaine à envoyer au client
+		*/
+		public function getMessageDrawCard($joueurSuivant=0,$joueurs=array()){
+			$message="";
+		
+			foreach($joueurs as $joueur){
+				if($joueur->getNom()!= $this->getNom()){
+					/*pour chaque joueur on met sa carte actuel, et sont nombre total de carte*/
+					$message += $joueur->getActualCard()."-".$joueur->getAllCards()."-";
+				}
+				/*si je suis le joueur suivant*/
+				if($joueurSuivant == $this->getNom()){
+					/*vu que c'est le joueur suivant ou lui affecte PLAY pour qu'il puisse jouer*/
+					$message+="-PLAY";
+				}
+			}
+			return $message;
+		}
+		
+		/*
+		* Renvoie le message correspondant de type : 
+		* Param la liste de carte des joueurs
+		*/
+		public function getMessageCatch($joueurCatcheur,$joueursCatcher,$joueurs){
+			$tab=array();
+			$message="";
+			/*si le catch et gagné*/
+			if($joueursCatcher != ""){
+				/*si il y a plus d'un joueur*/
+				if(count($joueursCatcher) > 1){
+					/*foreach($joueursCatcher as $joueur){
+						array_push($tab,$joueur->getCardsKnown());
+					}
+					$joueurCatcheur->setAfterCatch($tab);
+					foreach($joueurs as $joueur){}*/
+					
+				}else{
+					$joueursCatcher->setAfterCatch($joueurCatcheur->getCardsKnown());
+					foreach($joueurs as $joueur){
+						$message += $joueur->getActualCard()."-".$joueur->getAllCards()."-";
+					}
+				}
+			/*Si le catch est raté*/
+			}else{
+				/*foreach($joueursCatcher as $joueur){
+					array_push($tab,$joueur->getCardsKnown());
+				}*/
+				$joueurCatcheur->setAfterCatch($joueursCatcher->getCardsKnown());
+				
+				foreach($joueurs as $joueur){
+					$message += $joueur->getActualCard()."-".$joueur->getAllCards()."-";
+				}
+			}
+		}
+	}
+	
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PéHpé/Partie.php
@@ -0,0 +1,295 @@
+﻿<?php
+//#!/usr/local/bin/php -q
+error_reporting(E_ALL);
+
+set_time_limit (0);
+
+	/*----------------------------------------------------------------PARTIE--------------------------------------------------------------------------------*/
+	
+	class Partie{		
+		public $socketServer;
+		private $joueurActif;
+		private $joueurSuivant;
+		private $joueurs = array();
+		private $maxClient; 
+		private $address = '127.0.0.1';
+		private $port = 1002;
+		private $cartes = array();
+		private $message;
+			
+		public function __construct(){
+			$this->joueurActif = 0;
+			$this->joueurSuivant = 1;
+			$this->maxClient = 1;
+			$this->waitClient();
+		}
+		
+		public function __destruct(){
+	
+		}	
+		
+		/*
+		*	Demarage du serveur
+		*/
+		public function startServer(){
+
+			if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+				echo "socket_create() a échoué : raison : " . socket_strerror(socket_last_error()) . "\n";
+			} 
+
+			if (socket_bind($sock, $this->address, $this->port) === false) { 
+				echo "socket_bind() a échoué : raison : " . socket_strerror(socket_last_error($sock)) . "\n";
+			} 
+			
+			if (socket_listen($sock, 5) === false) {
+				echo "socket_listen() a échoué : raison : " . socket_strerror(socket_last_error($sock)) . "\n";
+			}
+			
+			return $sock;
+		}
+		
+		/*
+		* Attente de connection des clients
+		*/
+		function waitClient(){					
+			set_time_limit(0);
+			$clients = array();
+			
+			/*demarage du serveur*/
+			$this->socketServer = $this->startServer();
+			
+			/*Accepte client */
+			for($i=0;$i<$this->maxClient;$i++){
+			
+				if(($client = socket_accept($this->socketServer)) === false){
+					 echo "socket_accept() a échoué : raison : " . socket_strerror(socket_last_error($this->socketServer)) . "\n";
+					 break;
+				}else echo "<br/>Client $client[$i] has connected\n";
+				
+				/*on stock les clients(socket) dans un membre de l'objet joueur*/
+				$this->joueurs[$i] = new Joueur($client,$i);
+				
+				//socket_close($client);
+			}
+			
+			$this->game();
+		}
+		
+		/*******************************************************************GAME***************************************************************************/
+		
+		/*
+		* Game
+		*/
+		public function game(){
+			$start=true;
+			$this->initDeck();
+			$this->addCardToPlayers();
+			$this->writeToPlayers(0,"","");
+			
+			/*debut du jeu*/
+			while($start){
+					$messageClient="";
+					
+					//$numChanged = socket_select($clients, $write, $except, 0);
+				for($i=0;$i<$this->maxClient;$i++){	
+					
+					$socket = $this->joueurs[$i]->getSocket();
+					
+					/*on set les options necessaire au non bloquage du read*/
+					socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1, "usec"=>0));
+					socket_set_nonblock($socket);
+					
+					//if(false !== ($bytes = socket_recv($this->joueurs[$i]->getSocket(), $messageClient, 2048,MSG_DONTWAIT))){
+					$messageClient = socket_read($socket, 1024);
+								
+						/*si le message du client est vide (improbable) on traite*/
+						if($messageClient != ""){
+							
+							/*si c'est un catch*/
+							if($this->hasMessageClientCatch()){
+								$joueurCatcheur="";
+								$lstJoueursCatch;
+								$requeteC="";
+								
+								$joueurCatcheur = $this->getJoueurCatcheur($messageClient);
+								
+								/*prepare la requete pour le C*/
+								$requeteC = $this->reqPrepareForC();
+								
+								/*Lande le module c*/
+								$lstJoueursCatcher = lancerModuleC($requeteC);
+								
+								/*affectation des joueurs*/
+								$lstJoueursCatcher = $this->tradRequeteC($requeteC);
+								
+								/*envoie la mise a jour aux joueurs en fonction de la reponse du C en donnant l'etat 2 : catch*/
+								$this->writeToPlayers(2,$joueurCatcheur,$lstJoueursCatch);
+							
+							/*sinon c'est forcement pour tiré une carte*/
+							}else{
+								/*envoie mise a jour aux joueurs en donnant l'etat 1 : tirer carte*/
+								//$this->writeToPlayers(1,"","");
+								/*On met à jour le nouveau joueur actif et le joueur suivant*/
+								//$this->majJoueurActif();
+							}
+						}
+					}
+				 //echo "erreur : ".socket_strerror(socket_last_error());
+				//socket_close($this->socketServer);
+			}
+		}
+		/*
+		* envoie un message particulier aux joueurs
+		*/
+		public function writeToPlayers($etat=0,$joueurCatcheur,$lstJoueursCatcher){
+			
+			switch($etat){
+				/*init*/
+				case 0 :
+						/*pour chaque joueurs on envoie le message lui etant dédié*/			
+						foreach($this->joueurs as $joueur){
+							$message = $joueur->getMessageInit($this->joueurActif);
+							socket_write($joueur->getSocket(),$message."\n",strlen($message."\n"));
+						}
+					break;
+				/*tirage carte*/
+				case 1 :
+						/*mise à jours des cartes du joueur actif*/
+						$this->joueurs[$this->joueurActif]->drawCard();
+						
+						/*pour chaque joueurs on envoie le message lui etant dédié*/			
+						foreach($this->joueurs as $joueur){
+							$message = $joueur->getMessageDrawCard($this->joueurSuivant,$this->joueurs);
+							socket_write($joueur->getSocket(),$message."\n",strlen($message."\n"));
+							fflush($joueur->getSocket());
+						}
+					break;
+				/*catch*/
+				case 2 :
+						exit(0);
+						/*pour chaque joueurs on envoie le message lui etant dédié*/			
+						foreach($this->joueurs as $joueur){
+							$message = $joueur->getMessageCatch($joueurCatcheur,$lstJoueursCatcher,$this->joueurs);
+							socket_write($joueur->getSocket(),$message."\n",strlen($message."\n"));
+							fflush($joueur->getSocket());
+						}						
+					break;
+				default : echo "!!!!!!!!!!!!! erreur !!!!!!!!!!!!!";
+			}		
+		}
+
+		/***************************************************************************************************************************************************/
+		
+		
+		/*
+		* Prepare la requete qui va etres envoyer au C pour verifification
+		* retour : String requete
+		*/
+		public function reqPrepareForC(){
+			foreach($this->joueurs as $joueur){
+				$requeteC += $joueur->getActualCard()+"-";
+			}
+			return $requeteC;
+		}
+		
+		/*
+		* traduit la requete envoyer par le C
+		* param : reponde requete de type : "numjoueur-numjoueur"
+		* return : renvoie une string ou un array de joueur(s) (traduction de la reponse C)
+		*/
+		public function tradRequeteC($req){
+			$rep;
+			$tabJ = array();
+			$rep = explode("-",$req);
+			
+			switch(count($rep)){
+				case 0 : 
+					return "";
+					break;
+				case 1 : 
+					return $this->joueurs[$rep];
+					break;
+				case 2 : 
+					array_push($tabJ,$this->joueurs[$rep[0]]);
+					array_push($tabJ,$this->joueurs[$rep[1]]);
+					return $tabJ;
+					break;
+				case 3 : 
+					array_push($tabJ,$this->joueurs[$rep[0]]);
+					array_push($tabJ,$this->joueurs[$rep[1]]);
+					array_push($tabJ,$this->joueurs[$rep[2]]);
+					return $tabJ;
+					break;
+			}  
+		}
+		
+		/*
+		* Lance le module c
+		* param : la requete préparé
+		*/
+		public function lancerModuleC($requete){
+			//$lastLine = system("./Jungle ".$req." ",$return);
+			//return $lastLine;
+			return "";
+		}
+		
+		/*
+		* verifie si le message provenant du client et du au catch
+		* retour : vrai si c'est un catch, faux si non
+		*/
+		public function hasMessageClientCatch($message=""){
+			$resultat = explode("-",$message);
+			if($resultat[0] == "catch"){
+				return true;
+			}else return false;
+		}
+		
+		/*
+		* Renvoie le joueur qui a catcher
+		* param : le message lu par read
+		*/
+		public function getJoueurCatcheur($messageClient){
+			$resultat = 0;
+			$resultat = explode("-",$messageClient);
+			return $this->joueurs[$resultat[1]];
+		}
+		
+		/*
+		* Met à jour le joueur actif et le joueur suivant
+		*/
+		public function majJoueurActif(){
+			$this->joueurActif = $this->joueurSuivant;
+			$this->joueurSuivant = ($this->joueurActif == $this->maxClient-1 ? 0 : $this->joueurActif+1);
+		}
+		
+		/*----------------------------------------------------------Fonction lié a la gestion des cartes-----------------------------------------------*/
+		
+		/*
+		* Initialisation du paquet de cartes
+		*/
+		public function initDeck(){
+			$j=1;
+			for($i=1;$i<81;$i++){
+				$this->cartes[$i] = $j;
+				if ($j == 20){
+					$j = 1;
+				}else $j++;
+			}
+		}
+		
+		/*
+		* Ajout des cartes aux joueurs
+		*/
+		public function addCardToPlayers(){
+		
+			/*On melange les cartes*/
+			shuffle($this->cartes);			
+			
+			/*Affectation des cartes aux joueurs*/
+			$this->joueurs[0]->setCardsHide(array_slice($this->cartes,0,-60));
+			//$this->joueurs[1]->setCardsHide(array_slice($this->cartes,21,-39));
+			//$this->joueurs[2]->setCardsHide(array_slice($this->cartes,41,-19));
+			//$this->joueurs[3]->setCardsHide(array_slice($this->cartes,60));
+		}
+	}
+?>
